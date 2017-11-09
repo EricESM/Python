fccd33a1236820390f7a89874cd32636944d64ed
Eric Huang <erichuang@nevada.unr.edu>
2017-11-09 11:15:34 -0800

    project

---
+++ class_PA/CS135/PA01/PA01.py
@@ -0,0 +1,24 @@
+import csv
+import numpy as np
+
+
+'''Main'''
+col = 0;
+row = 0;
+temp = np.empty = ((0));
+'''matrix = [col][row];'''
+
+print("Enter the Size of the matrix: ");
+
+col = input('col: ');
+row = input('row: ');
+
+'''print(col, row);'''
+
+array = np.empty = ((0));
+for i in range((col * row)):
+	'''temp[0] = input();'''
+	'''array.append(array,temp);'''
+	'''print(temp);'''
+	temp = input();
+	array.append(array,temp);
